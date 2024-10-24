156c6be2f88b1da097307f5e109209a31a056c7b
Maxi Tromer <maxitromer@gmail.com>
2024-10-24 12:28:00 -0300

    improved script

--- helm/installs/from-hub/moco/03-db-and-user/db-and-user.sh
+++ helm/installs/from-hub/moco/03-db-and-user/db-and-user.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# Prompt for the namespace
-read -p "Enter the database namespace: " NAMESPACE
+# Ask if user wants to use defaults values for cluster name and namespace
+read -p "Do you want to use the default namespace (databases) and cluster name (mysql-cluster)? (y/n): " USE_DEFAULT_CLUSTER
 
-# Prompt for the cluster name
-read -p "Enter the cluster name: " CLUSTER_NAME
-
+if [[ $USE_DEFAULT_CLUSTER =~ ^[Yy]$ ]]; then
+    NAMESPACE="databases"
+    CLUSTER_NAME="mysql-cluster"
+else
+    read -p "Enter the cluster name: " CLUSTER_NAME
+fi
 # Prompt for a unique value
 read -p "Enter a unique value for user and database names: " UNIQUE_VALUE
 
 # Generate a random password
 PASSWORD=$(openssl rand -base64 12)
+# After getting the UNIQUE_VALUE, sanitize it
+UNIQUE_VALUE=$(echo "$UNIQUE_VALUE" | tr '-' '_')
 
 # Regenerate in base64 format 
 PASSWORD_IN_BASE64=$(echo -n "$PASSWORD" | base64)
 
-# Set variables using the unique value
+# Set variables using the sanitized unique value
 USER_NAME="${UNIQUE_VALUE}_user"
 DB_NAME="${UNIQUE_VALUE}_database"
 
@@ -29,7 +34,7 @@ kubectl moco -n "$NAMESPACE" mysql -u moco-writable "$CLUSTER_NAME" -- -e "CREAT
 kubectl moco -n "$NAMESPACE" mysql -u moco-writable "$CLUSTER_NAME" -- -e "GRANT ALL ON $DB_NAME.* TO '$USER_NAME'@'%'"
 
 # Display the generated user, database, and password
-echo "Created user: $USER_NAME"
-echo "Created database: $DB_NAME"
-echo "Generated password: $PASSWORD"
-echo "Generated password in base64 : $PASSWORD_IN_BASE64"
+echo "dbName: $DB_NAME"
+echo "username: $USER_NAME"
+echo "secretPasswordKey: $PASSWORD_IN_BASE64 # base64 format"
+echo "# actual password: $PASSWORD"
